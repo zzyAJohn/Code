2e9433d0323680517ef802008da26b0d7e10d802
AJohn <1833302139@qq.com>
2022-09-05 16:16:33 +0800

    四种对象

---
+++ CODE_Python/Python_Single/StudyPython/列表元组字典集合.py
@@ -0,0 +1,9 @@
+x_list = [1, 2, 3]                  #创建列表对象
+x_tuple = (1, 2, 3)                 #创建元组对象
+x_dict = {'a':97, 'b':98, 'c':99}   #创建字典对象
+x_set = {1, 2, 3}                   #创建集合对象
+
+print(x_list[0])              #1     序号访问
+print(x_tuple[0])             #1     序号访问
+print(x_dict['a'])            #97    键访问
+print(3 in x_set)             #True  成员测试
